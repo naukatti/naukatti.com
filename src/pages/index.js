0a9a42e6ce5332a944c834cf7e697c03fa022003
naukatti <noora.peltok@gmail.com>
2020-09-17 20:17:05 +0300

    updated description and css

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people ❤</h1>
+    <p>This site will someday hold my art porfolio, my thoughts and journey as a junior programmer</p>
     <p>Site under construction!</p>
   </Layout>
 )
